b75c886781e1905c65f7b9103db26f38e01af461
Muhammad Bilal <withbilal777@gmail.com>
2024-06-25 16:35:29 +0500

    redux-toolkit added, todoSlice created  with addTodo, removeTodo, toggleComplete, editTodo reducers

---
+++ src/features/todoSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.filter((todo) => todo.id !== action.payload);
+    },
+
+    toggleComplete: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+
+    editTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleComplete, editTodo } =
+  todoSlice.actions;
+
+export default todoSlice.reducer;
